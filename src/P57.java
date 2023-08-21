767b37165a95ebefb6ef0036bb262bb116af8b39
Awe03 <shans29008@gmail.com>
2023-08-21 19:37:27 +0530

    Add upto 59

---
+++ src/P57.java
@@ -0,0 +1,31 @@
+// Write a program in java to create a 5x3 matrix and store integer numbers using scanner class. Find the sum of all the elements
+// stored in each individual row and each individual column. Display the sum for each row and each column
+import java.util.Scanner;
+
+class P57 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[5][3];
+        int rowSum, colSum;
+        System.out.println("Enter 15 elements: ");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 3; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < 5; i++) {
+            rowSum = 0;
+            for(int j = 0; j < 3; j++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println("Sum of row " + (i + 1) + " is " + rowSum);
+        }
+        for(int i = 0; i < 3; i++) {
+            colSum = 0;
+            for(int j = 0; j < 5; j++) {
+                colSum += arr[j][i];
+            }
+            System.out.println("Sum of column " + (i + 1) + " is " + colSum);
+        }
+    }
+}
---
+++ src/P59.java
@@ -0,0 +1,28 @@
+// Write a program in java to create a 4x4 matrix and store integer numbers using scanner class. Find the sum of the elements stored in
+// the diagonal positions (from right to left). Display the sum. Also display the diagonal elements (right to left) in matrix form.
+import java.util.Scanner;
+
+class P59 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[4][4];
+        int sum = 0;
+        System.out.println("Enter 16 elements: ");
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < 4; i++) {
+            for(int j = 3; j >= 0; j--) {
+                if (i + j == 3) {
+                    sum += arr[i][j];
+                    System.out.print(" " + arr[i][j] + " ");
+                }
+                else System.out.print(" X ");
+            }
+            System.out.println();
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
