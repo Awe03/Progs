242fe765ffc38886675879bf3baa0135c06ad634
Awe03 <shans29008@gmail.com>
2023-08-21 20:49:34 +0530

    Cleanup

--- src/P57.java
+++ src/P57.java
@@ -6,7 +6,6 @@ class P57 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[5][3];
-        int rowSum, colSum;
         System.out.println("Enter 15 elements: ");
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 3; j++) {
@@ -14,14 +13,14 @@ class P57 {
             }
         }
         for(int i = 0; i < 5; i++) {
-            rowSum = 0;
+            int rowSum = 0;
             for(int j = 0; j < 3; j++) {
                 rowSum += arr[i][j];
             }
             System.out.println("Sum of row " + (i + 1) + " is " + rowSum);
         }
         for(int i = 0; i < 3; i++) {
-            colSum = 0;
+            int colSum = 0;
             for(int j = 0; j < 5; j++) {
                 colSum += arr[j][i];
             }
