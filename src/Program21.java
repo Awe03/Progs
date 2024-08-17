b3f8d596ea9a7895b683b966dc985857fe9202eb
Awe <shans29008@gmail.com>
2024-08-17 05:54:08 +0000

    Fixes

--- src/Program21.java
+++ src/Program21.java
@@ -1,56 +1,58 @@
-// Write a program to create a text file and accept information from the user. Write a program to read the contents of the previously created file
-
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.util.Scanner;
 
 class WriteInput {
-    public static void writeToFile() {
+    public static void writeToFile() throws IOException {
+        FileWriter fileWriter = new FileWriter("input.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        PrintWriter printWriter = new PrintWriter(bufferedWriter);
+        Scanner sc = new Scanner(System.in);
         try {
-            FileWriter fileWriter = new FileWriter("input.txt");
-            // Accept input from the user
-            Scanner sc = new Scanner(System.in);
             System.out.println("Enter the text to write to the file (type ':qa!' to quit):");
             String input = sc.nextLine();
             while (!input.equals(":qa!")) {
-                // Write the input to the file
-                PrintWriter printWriter = new PrintWriter(fileWriter);
                 printWriter.println(input);
-                printWriter.flush();
                 input = sc.nextLine();
             }
-            fileWriter.close();
-            sc.close();
             System.out.println("Successfully written to the file");
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("An error occurred");
             e.printStackTrace();
-        }
+        } finally {
+            printWriter.close();
+            bufferedWriter.close();
+            fileWriter.close();
+            sc.close();
+        }        
     }
 }
 
 class WriteOutput {
-    public static void readFromFile() {
+    public static void readFromFile() throws IOException {
+        FileReader fileReader = new FileReader("input.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
         try {
-            FileReader fileReader = new FileReader("input.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
-            fileReader.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("An error occurred");
             e.printStackTrace();
+        } finally {
+            fileReader.close();
+            bufferedReader.close();
         }
     }
 }
 
 class Program21 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WriteInput.writeToFile();
         WriteOutput.readFromFile();
     }
